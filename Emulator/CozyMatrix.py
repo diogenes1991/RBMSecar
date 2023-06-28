9ca629572af75b51e6e5bda3f8177958437df050
diogenes1991 <diogenes1991@gmail.com>
2023-06-28 15:24:56 -0400

    Update

---
+++ Emulator/CozyMatrix.py
@@ -0,0 +1,23 @@
+class CozyMatrix:
+    def __init__(self,path):
+        file = open(path,"r")
+        self.matrices = []
+        local_mat = 330*[0]
+        count = 1
+        for line in file:
+            count += 1
+            vec = line.split()
+            if vec[0] == "L" or vec[0] == "P" or vec[0] == "A" or vec[0][0] == "-":
+                continue
+            if vec[0] == "I" and len(local_mat)!=0:
+                self.matrices.append(local_mat)
+                #print(local_mat)
+                local_mat = 330*[0]
+            elif vec[0] != "I":
+                word = ""
+                for i in range(3,len(vec)):
+                    word+=vec[i]
+                try:
+                    local_mat[lookup[word]-1] = float(vec[1])
+                except:
+                    pass     
