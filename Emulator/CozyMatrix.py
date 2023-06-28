e5c877ae4e61bbb86bbc5891fbdc0cc452a7a074
diogenes1991 <diogenes1991@gmail.com>
2023-06-28 16:14:42 -0400

    BigMatrix

--- Emulator/CozyMatrix.py
+++ Emulator/CozyMatrix.py
@@ -2,7 +2,7 @@ class CozyMatrix:
     def __init__(self,path):
         file = open(path,"r")
         self.matrices = []
-        local_mat = 330*[0]
+        local_mat = 331*[0]
         count = 1
         for line in file:
             count += 1
@@ -12,7 +12,7 @@ class CozyMatrix:
             if vec[0] == "I" and len(local_mat)!=0:
                 self.matrices.append(local_mat)
                 #print(local_mat)
-                local_mat = 330*[0]
+                local_mat = 331*[0]
             elif vec[0] != "I":
                 word = ""
                 for i in range(3,len(vec)):
@@ -20,4 +20,4 @@ class CozyMatrix:
                 try:
                     local_mat[lookup[word]-1] = float(vec[1])
                 except:
-                    pass     
+                    print(    
