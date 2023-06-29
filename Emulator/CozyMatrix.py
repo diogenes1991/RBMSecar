9f263b2f595b608a3c16bfc1884e0fb5abfeca71
diogenes1991 <diogenes1991@gmail.com>
2023-06-28 23:30:03 -0400

    Emulator class

--- Emulator/CozyMatrix.py
+++ Emulator/CozyMatrix.py
@@ -1,3 +1,5 @@
+from lookup import lookup
+
 class CozyMatrix:
     def __init__(self,path):
         file = open(path,"r")
@@ -7,17 +9,21 @@ class CozyMatrix:
         for line in file:
             count += 1
             vec = line.split()
-            if vec[0] == "L" or vec[0] == "P" or vec[0] == "A" or vec[0][0] == "-":
+            
+            ''' Skip specification lines '''
+            if vec[0] == "L" or vec[0] == "P" or vec[0] == "A" or vec[0] == "I":
                 continue
-            if vec[0] == "I" and len(local_mat)!=0:
+            
+            ''' Append at the end of matrix character '''
+            if vec[0][0] == "-":
                 self.matrices.append(local_mat)
-                #print(local_mat)
                 local_mat = 331*[0]
-            elif vec[0] != "I":
+            
+            else:
                 word = ""
                 for i in range(3,len(vec)):
                     word+=vec[i]
                 try:
                     local_mat[lookup[word]-1] = float(vec[1])
                 except:
-                    print(    
+                    print(word,count)     
